28b23290741d3690f404dea01112ce1c9ef47bd3
Amit <amore43035@gmail.com>
2025-06-27 21:45:29 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import Login from './components/User/UserLogin'
+import Chat from '../pages/User/Chat'
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+ 
+
+ 
+
+  return (
+    <Router>
+    <Routes>
+      {/* Public Routes (No Sidebar) */}
+      <Route path="/" element={<Login />} />
+      <Route path="/chat" element={<Chat />} />
+      
+      </Routes>
+      </Router>
+  )
+}
+
+export default App;
+
