fb6d72f39d9ed061f9b83896abb62609f1d79d5d
Kurian Tony Aloor <es.intern106@ecerasystem.com>
2025-06-29 17:41:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,20 @@
-import { useState } from 'react'
-import Login from './components/User/UserLogin'
-import verificationSuite from './components/User/verification_suite'
-import Chat from './pages/User/Chat'
-
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/User/UserLogin";
+import Chat from "./pages/User/Chat";
+import UserVerificationDashboard from "./UserVerificationDashboard";
 
 function App() {
- 
-
- 
-
   return (
     <Router>
-    <Routes>
-      {/* Public Routes (No Sidebar) */}
-      <Route path="/" element={<Login />} />
-      <Route path="/chat" element={<Chat />} />
-      <Route path="/verify" element={<verificationSuite />} />
-      
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/verify" element={<UserVerificationDashboard />} />
       </Routes>
-      </Router>
-  )
+    </Router>
+  );
 }
 
 export default App;
-
