2c53496a6406bedad141df4152c8bf1ed20d5f8a
rajdesire <rks4in@gmail.com>
2025-07-04 09:59:24 -0400

    Assignment

--- backend/routes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-// User profile routes
-import { getAllUsers, getUserById } from "../controllers/userController.js";
-import express from "express";
-const userRouter = express.Router();
-
-userRouter.get("/api/v1/users", getAllUsers);
-userRouter.get("/api/v1/users/:id", getUserById);
-
-export default userRouter;
--- backend/services/emailService.js
+++
@@ -1,1 +0,0 @@
-// Email templates and sending
